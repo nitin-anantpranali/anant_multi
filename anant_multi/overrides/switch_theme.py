e6005e7be2ac52a657bc1a7d48e236d4bde716ec
nitin-anantpranali <116695322+nitin-anantpranali@users.noreply.github.com>
2024-12-31 10:48:24 +0530

    1.0.9
    Changed Default Theme Colour

--- anant_multi/overrides/switch_theme.py
+++ anant_multi/overrides/switch_theme.py
@@ -2,5 +2,5 @@ import frappe
 
 @frappe.whitelist()
 def switch_theme(theme):
-	if theme in ["Dark", "Light", "Automatic", "Anant_red_theme", "Anant_blue_theme", "Anant_orange_theme", "Anant_green_theme"]:
+	if theme in ["Dark", "Light", "Automatic", "Anant_blue_theme", "Anant_red_theme", "Anant_orange_theme", "Anant_green_theme",]:
 		frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
